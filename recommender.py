740765515c0e0440657c219a326744d2dc9837e2
yashpandey474 <ypandey474@gmail.com>
2023-07-01 00:24:02 +0530

    Updated recommender and removed DS_store

--- recommender.py
+++ recommender.py
@@ -58,3 +58,30 @@ nb_users = int(max(max(training_set[:,  0]), max(test_set[:, 0])))
 max_training = np.max(training_set[:, 1])
 max_test = np.max(test_set[:, 1])
 nb_movies = int(max(max_training, max_test))
+
+
+#CONVERTING DATA INTO ARRAY WITH MOVIES AS COLUMNS AND USERS IN ROWS: USUAL STRUCTURE OF DATA FOR NEURAL NETWORKS & RBMs
+def convert(data): #PASS TRAINING AND TEST SET
+    #LIST OF LISTS: ONE FOR EACH USER
+    new_data = []
+    
+    for id_users in range(1, nb_users+1):
+        
+         #ALL IDS OF MOVIES RATED BY THIS USER
+        id_movies = data[:, 1][data[:, 0] == id_users]
+             
+             #ALL RATINGS GIVEN BY THIS USER
+        id_ratings = data[:, 2][data[:, 0] == id_users]
+             
+        ratings = np.zeros(nb_movies)
+             #SET THE RATED MOVIES TO THE CORRESPONDING RATING
+        ratings[id_movies-1] = id_ratings
+             
+        new_data.append(list(ratings))
+         
+    return new_data
+
+training_set = convert(training_set)
+test_set = convert(test_set)
+
+
